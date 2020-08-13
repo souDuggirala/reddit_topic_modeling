37e5daf1edcf2e91a9031c0f1db782cb1e47557f
Soumya Duggirala <soumyadugg@soumyas-mbp.home>
2020-08-13 15:42:21 -0400

    Edited to allow for missing documents and to write over previous textfiles if necessary

--- make_char_min_dir.py
+++ make_char_min_dir.py
@@ -1,25 +1,28 @@
 import io
 import os
 
-charlim = 3000
+charmin = 2000
 
 parent_dir = '/Users/soumyadugg'
 dir_name = 'legal_advice_files'
 dir_path = os.path.join(parent_dir, dir_name + '_cleaned')
 
-char_lim_dir = os.path.join(parent_dir, dir_name + '_charlim{}'.format(charlim))
-if not os.path.isdir(char_lim_dir):
-    os.mkdir(char_lim_dir)
-#else delete all files in directory
+char_min_dir = os.path.join(parent_dir, dir_name + '_charmin{}'.format(charmin))
+if not os.path.isdir(char_min_dir):
+    os.mkdir(char_min_dir)
 
 k = 0
-for i in range(600,720):
+for i in range(600,1223):
     for j in range(100):
-        file = open(os.path.join(dir_path, 'doc{}-{}.txt'.format(i,j)), 'r')
+        file_path = os.path.join(dir_path, 'doc{}-{}.txt'.format(i,j))
+        if not os.path.exists(file_path):
+            continue
+        file = open(file_path, 'r')
         content = file.read()
-        if len(content) >= charlim :
-            #write to new directory
-            new_file = open(os.path.join(char_lim_dir,'doc{}.txt'.format(k)), 'w')
+        if len(content) >= charmin :
+            new_file = open(os.path.join(char_min_dir,'doc{}.txt'.format(k)), 'w')
+            new_file.seek(0)
+            new_file.truncate(0)
             new_file.write(content)
             new_file.close()
             k+=1
